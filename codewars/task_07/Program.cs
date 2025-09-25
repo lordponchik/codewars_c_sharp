134b4188eba1b3c50c787232bb5e31a6b32ea353
Denys Strashniuk <d.strashniuk@gmail.com>
2025-09-25 17:14:59 +0200

    hinzufügen: task_07

---
+++ codewars/task_07/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+7 lvl. Regexp-Grundlagen – ist es ein Vokal?
+Implementieren Sie die Funktion, die „true“ zurückgeben soll, wenn das angegebene Objekt ein Vokal ist (also a, e, i, o, u, Groß- oder Kleinbuchstabe), 
+und andernfalls „false“.
+
+Reguläre AusdrückeGrundlagen
+*/
+
+using System.Text.RegularExpressions;
+
+namespace task_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Vowel("Niks")); // false;
+            Console.WriteLine(Vowel("")); //  false
+            Console.WriteLine(Vowel("e")); //  true
+            Console.WriteLine(Vowel("E")); //  true
+        }
+
+        static bool Vowel(string s) => s.Length > 1 ? false : Regex.IsMatch(s.ToLower(), "[aeiou]");
+    }
+}
