a58b3c5d9bc85bcf8028e0243c30e19432d6a0e3
Denys Strashniuk <d.strashniuk@gmail.com>
2025-09-25 18:05:25 +0200

    hinzufügen: task_23

---
+++ codewars/task_23/Program.cs
@@ -0,0 +1,28 @@
+﻿/* 
+7 lvl. Unscrambled eggs
+Unscramble the eggs.
+
+The string given to your function has had an "egg" inserted directly after each consonant. You need to return the string before it became eggcoded.
+
+Example
+unscrambleEggs("Beggegeggineggneggeregg")  =>  "Beginner"
+//             "B---eg---in---n---er---"
+Kata is supposed to be for beginners to practice reggular eggspressions, so commenting would be appreciated.
+
+Fundamentals
+*/
+
+namespace task_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UnscrambleEggs("Beggegeggineggneggeregg")); // "Beginner"
+            Console.WriteLine(UnscrambleEggs("ceggodegge heggeregge")); //"code here"
+            Console.WriteLine(UnscrambleEggs("FeggUNegg KeggATeggA")); //"FUN KATA"
+        }
+
+        static string UnscrambleEggs(string word) => word.Replace("egg", "");
+    }
+}
