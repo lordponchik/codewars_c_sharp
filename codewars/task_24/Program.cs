139941eb34aedf8b1b2205ce9ea273de4121d50b
Denys Strashniuk <d.strashniuk@gmail.com>
2025-09-25 18:06:34 +0200

    hinzufügen: task_24

---
+++ codewars/task_24/Program.cs
@@ -0,0 +1,65 @@
+﻿/*
+7 lvl. Rotate for a Max
+Let us begin with an example:
+
+Take a number: 56789. Rotate left, you get 67895.
+
+Keep the first digit in place and rotate left the other digits: 68957.
+
+Keep the first two digits in place and rotate the other ones: 68579.
+
+Keep the first three digits and rotate left the rest: 68597. Now it is over since keeping the first four it remains only one digit which rotated is itself.
+
+You have the following sequence of numbers:
+
+56789 -> 67895 -> 68957 -> 68579 -> 68597
+
+and you must return the greatest: 68957.
+
+Task
+Write function max_rot(n) which given a positive integer n returns the maximum number you got doing rotations similar to the above example.
+
+So max_rot (or maxRot or ... depending on the language) is such as:
+
+max_rot(56789) should return 68957
+
+max_rot(38458215) should return 85821534
+
+Algorithms
+*/
+
+namespace task_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxRot(56789)); // 68597
+            Console.WriteLine(MaxRot(38458215)); //85821534
+            Console.WriteLine(MaxRot(896219342)); //962193428
+        }
+
+        static long MaxRot(long n)
+        {
+            int[] arr = n.ToString().Select(x => Convert.ToInt32(x.ToString())).ToArray();
+            long[] outputArr = new long[arr.Length];
+
+            outputArr[0] = long.Parse(String.Join("", arr));
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int temp = arr[i];
+
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (j == arr.Length - 1) arr[j] = temp;
+                    else arr[j] = arr[j + 1];
+                }
+
+                outputArr[i + 1] = long.Parse(String.Join("", arr));
+            }
+
+            return outputArr.Max();// your code
+        }
+    }
+}
