b3836afff6e30b2719b92f55d6b362639ba72064
Denys Strashniuk <d.strashniuk@gmail.com>
2025-09-29 07:58:53 +0200

    hinzufügen: task_29

---
+++ codewars/task_29/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ 7 lvl - Eindeutige Zeichenketten
+In diesem Kata erhalten Sie zwei Zeichenketten a und b. Ihre Aufgabe besteht darin, die Zeichen zu ermitteln, die in den beiden Zeichenketten nicht vorkommen.
+
+Beispiel:
+
+solve("xyab","xzca") = "ybzc"
+– Die erste Zeichenkette enthält das Wort „yb“, das in der zweiten nicht vorkommt.
+– Die zweite Zeichenkette enthält das Wort „zc“, das in der ersten nicht vorkommt.
+
+Beachten Sie außerdem, dass die Zeichen der ersten Zeichenkette mit denen der zweiten Zeichenkette verknüpft zurückgegeben werden.
+
+Weitere Beispiele finden Sie in den Testfällen.
+
+Viel Erfolg!
+
+Versuchen Sie auch, Duplikate einfach zu entfernen.
+
+StringsFundamentals
+ */
+
+namespace task_29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve("xyab", "xzca")); // ybzc
+            Console.WriteLine(Solve("abcd", "xyz")); // abcdxyz
+        }
+
+        static string Solve(string a, string b)
+        {
+            string ergebnis = "";
+
+            foreach (var item in a)
+                if (!b.Contains(item)) ergebnis += item;
+
+            foreach (var item in b)
+                if (!a.Contains(item)) ergebnis += item;
+
+            return ergebnis;
+        }
+    }
+}
